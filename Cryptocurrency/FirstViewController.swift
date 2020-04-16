17cd3d7619b70b674e754a1e57513a4b0f54583e
Owii <oiichrism03@gmail.com>
2020-04-16 15:21:56 +0700

    Initial Commit

---
+++ Cryptocurrency/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cryptocurrency
+//
+//  Created by Owii on 16/04/20.
+//  Copyright © 2020 DataEdukasi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cryptocurrency/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cryptocurrency
+//
+//  Created by Owii on 16/04/20.
+//  Copyright © 2020 DataEdukasi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
