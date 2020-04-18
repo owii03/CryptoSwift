2c5a460adccc4c0cb481d673a970469c3f48b9ff
Owii <oiichrism03@gmail.com>
2020-04-19 03:34:31 +0700

    added new dummy data and view of price hour and day

--- Cryptocurrency/Coin.swift
+++ Cryptocurrency/Coin.swift
@@ -13,4 +13,10 @@ struct Coin {
     let photo: UIImage
     let name: String
     let description: String
+    let price1hour: String
+    let label1hour: String
+    let price24hour: String
+    let label24hour: String
+    let price7day: String
+    let label7day: String
 }
--- Cryptocurrency/DetailViewController.swift
+++ Cryptocurrency/DetailViewController.swift
@@ -16,15 +16,33 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var descCoin: UILabel!
     
+    @IBOutlet weak var price1h: UILabel!
+    @IBOutlet weak var label1h: UILabel!
+    
+    
+    @IBOutlet weak var price24h: UILabel!
+    @IBOutlet weak var label24h: UILabel!
+    
+    @IBOutlet weak var price7d: UILabel!
+    @IBOutlet weak var label7d: UILabel!
+    
+    
+    
     var coin: Coin?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
        if let result = coin {
-           photoCoin.image = result.photo
-           nameCoin.text = result.name
-           descCoin.text = result.description
+            photoCoin.image = result.photo
+            nameCoin.text = result.name
+            descCoin.text = result.description
+            price1h.text = result.price1hour
+            label1h.text = result.label1hour
+            price24h.text = result.price24hour
+            label24h.text = result.label24hour
+            price7d.text = result.price7day
+            label7d.text = result.label7day
        }
     }
 }
